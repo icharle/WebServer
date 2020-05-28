0268ac8acc820e0f479f07d10e7db41cd01db925
icharle <icharle@live.com>
2020-05-28 15:55:37 +0800

    fix fastcgi bug

--- FastCgi.cpp
+++ FastCgi.cpp
@@ -71,131 +71,57 @@ int FastCgi::sendStartRequestRecord() {
     }
 }
 
-//int FastCgi::sendParams(char *name, char *value) {
-//    int nlen = strlen(name);
-//    int vlen = strlen(value);
-//    unsigned char *buf;
-//    int ret, bodylen = nlen + vlen;
-//    (nlen < 128) ? (++bodylen) : (bodylen + 4);
-//    (vlen < 128) ? (++bodylen) : (bodylen + 4);
-//
-//    buf = static_cast<unsigned char *>(malloc(bodylen + FASTCGI_HEADER_LENGTH));
-//
-//    std::cout << bodylen << std::endl;
-//
-//    // header
-//    FASTCGI_Header nameValueHeader = makeHeader(FASTCGI_PARAMS, requestId, bodylen, 0);
-//    memcpy(buf, (char *) &nameValueHeader, FASTCGI_HEADER_LENGTH);
-//    buf = buf + FASTCGI_HEADER_LENGTH;
-//    // header - end
-//
-//    // body
-//    /* 如果 nameLen 小于128字节 */
-//    if (nlen < 128) {
-//        *buf++ = (unsigned char) nlen; //nameLen用1个字节保存
-//    } else {
-//        /* nameLen 用 4 个字节保存 */
-//        *buf++ = (unsigned char) ((nlen >> 24) | 0x80);
-//        *buf++ = (unsigned char) (nlen >> 16);
-//        *buf++ = (unsigned char) (nlen >> 8);
-//        *buf++ = (unsigned char) nlen;
-//    }
-//
-//    /* valueLen 小于 128 就用一个字节保存 */
-//    if (vlen < 128) {
-//        *buf++ = (unsigned char) vlen;
-//    } else {
-//        /* valueLen 用 4 个字节保存 */
-//        *buf++ = (unsigned char) ((vlen >> 24) | 0x80);
-//        *buf++ = (unsigned char) (vlen >> 16);
-//        *buf++ = (unsigned char) (vlen >> 8);
-//        *buf++ = (unsigned char) vlen;
-//    }
-//
-//    memcpy(buf, name, nlen);
-//    buf = buf + nlen;
-//    memcpy(buf, value, vlen);
-//    // body - end
-//
-//    ret = writen(socketFd, buf, FASTCGI_HEADER_LENGTH + bodylen);
-//    if (ret == FASTCGI_HEADER_LENGTH + bodylen) {
-//        return 0;
-//    } else {
-//        std::cout << "sendParams" << std::endl;
-//        abort();
-//        return -1;
-//    }
-//}
+int FastCgi::sendParams(char *name, char *value) {
+    int nlen = strlen(name);
+    int vlen = strlen(value);
+    int ret, bodylen = nlen + vlen;
+    (nlen < 128) ? (++bodylen) : (bodylen + 4);
+    (vlen < 128) ? (++bodylen) : (bodylen + 4);
 
-int FastCgi::makeNameValueBody(char *name, int nameLen, char *value, int valueLen, unsigned char *bodyBuffPtr,
-                               int *bodyLenPtr) {
-    /* 记录 body 的开始位置 */
-    unsigned char *startBodyBuffPtr = bodyBuffPtr;
+    auto *buf = static_cast<unsigned char *>(malloc(bodylen + FASTCGI_HEADER_LENGTH));
 
+    // header
+    FASTCGI_Header nameValueHeader = makeHeader(FASTCGI_PARAMS, requestId, bodylen, 0);
+    memcpy(buf, (char *) &nameValueHeader, FASTCGI_HEADER_LENGTH);
+    // header - end
+
+    // body
+    unsigned char bodyBuff[bodylen];
+    int j = 0;
     /* 如果 nameLen 小于128字节 */
-    if (nameLen < 128) {
-        *bodyBuffPtr++ = (unsigned char) nameLen; //nameLen用1个字节保存
+    if (nlen < 128) {
+        bodyBuff[j++] = (unsigned char) nlen; //nameLen用1个字节保存
     } else {
         /* nameLen 用 4 个字节保存 */
-        *bodyBuffPtr++ = (unsigned char) ((nameLen >> 24) | 0x80);
-        *bodyBuffPtr++ = (unsigned char) (nameLen >> 16);
-        *bodyBuffPtr++ = (unsigned char) (nameLen >> 8);
-        *bodyBuffPtr++ = (unsigned char) nameLen;
+        bodyBuff[j++] = (unsigned char) ((nlen >> 24) | 0x80);
+        bodyBuff[j++] = (unsigned char) (nlen >> 16);
+        bodyBuff[j++] = (unsigned char) (nlen >> 8);
+        bodyBuff[j++] = (unsigned char) nlen;
     }
 
     /* valueLen 小于 128 就用一个字节保存 */
-    if (valueLen < 128) {
-        *bodyBuffPtr++ = (unsigned char) valueLen;
+    if (vlen < 128) {
+        bodyBuff[j++] = (unsigned char) vlen;
     } else {
         /* valueLen 用 4 个字节保存 */
-        *bodyBuffPtr++ = (unsigned char) ((valueLen >> 24) | 0x80);
-        *bodyBuffPtr++ = (unsigned char) (valueLen >> 16);
-        *bodyBuffPtr++ = (unsigned char) (valueLen >> 8);
-        *bodyBuffPtr++ = (unsigned char) valueLen;
+        bodyBuff[j++] = (unsigned char) ((vlen >> 24) | 0x80);
+        bodyBuff[j++] = (unsigned char) (vlen >> 16);
+        bodyBuff[j++] = (unsigned char) (vlen >> 8);
+        bodyBuff[j++] = (unsigned char) vlen;
     }
 
-    /* 将 name 中的字节逐一加入body中的buffer中 */
-    for (int i = 0; i < strlen(name); i++) {
-        *bodyBuffPtr++ = name[i];
+    for (size_t i = 0; i < strlen(name); i++) {
+        bodyBuff[j++] = name[i];
     }
 
-    /* 将 value 中的值逐一加入body中的buffer中 */
-    for (int i = 0; i < strlen(value); i++) {
-        *bodyBuffPtr++ = value[i];
+    for (size_t i = 0; i < strlen(value); i++) {
+        bodyBuff[j++] = value[i];
     }
+    memcpy(buf + FASTCGI_HEADER_LENGTH, bodyBuff, bodylen);
+    // body - end
 
-    /* 计算出 body 的长度 */
-    *bodyLenPtr = bodyBuffPtr - startBodyBuffPtr;
-    return 1;
-}
-
-int FastCgi::sendParams(char *name, char *value) {
-    int rc;
-
-    unsigned char bodyBuff[PARAMS_BUFF_LEN];
-
-    bzero(bodyBuff, sizeof(bodyBuff));
-
-    /* 保存 body 的长度 */
-    int bodyLen;
-
-    /* 生成 PARAMS 参数内容的 body */
-    makeNameValueBody(name, strlen(name), value, strlen(value), bodyBuff, &bodyLen);
-
-    FASTCGI_Header nameValueHeader;
-    nameValueHeader = makeHeader(FASTCGI_PARAMS, requestId, bodyLen, 0);
-    /*8 字节的消息头*/
-
-    int nameValueRecordLen = bodyLen + FASTCGI_HEADER_LENGTH;
-    char nameValueRecord[nameValueRecordLen];
-
-    /* 将头和body拷贝到一块buffer 中只需调用一次write */
-    memcpy(nameValueRecord, (char *) &nameValueHeader, FASTCGI_HEADER_LENGTH);
-    memcpy(nameValueRecord + FASTCGI_HEADER_LENGTH, bodyBuff, bodyLen);
-
-    rc = write(socketFd, nameValueRecord, nameValueRecordLen);
-    assert(rc == nameValueRecordLen);
-
+    ret = write(socketFd, buf, bodylen + FASTCGI_HEADER_LENGTH);
+    assert(ret == bodylen + FASTCGI_HEADER_LENGTH);
     return 1;
 }
 
@@ -256,13 +182,13 @@ char *FastCgi::recvRecord() {
     FASTCGI_Header respHeader{};
 
     int contentLen;
-    char content[2048];
+    static char content[2048];
     char paddingBuf[8];
 
     while (read(socketFd, &respHeader, FASTCGI_HEADER_LENGTH) > 0) {
         if (respHeader.type == FASTCGI_STDOUT) {
             contentLen = (respHeader.contentLengthB1 << 8) + (respHeader.contentLengthB0);
-            memset(content, 0, 2048);
+            memset(content, 0, contentLen);
 
             read(socketFd, content, contentLen);
 
